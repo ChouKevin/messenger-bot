256b4d50a18ebbb17b8c41a4527c9e0ed1aa28a1
ChouKevin <uuuu12tw@gmail.com>
2017-10-09 17:07:03 +0900

    model modify

--- fb_messenger_bot/model/comment.py
+++ fb_messenger_bot/model/comment.py
@@ -3,8 +3,6 @@ from mongoengine.fields import *
 
 class CommentQuerySet(QuerySet):
     """docstring for CommentQuerySet"""
-    def __init__(self):
-        super(CommentQuerySet, self).__init__()
     def search_by_uid(self, uid):
         return self.filter(uid=uid)
     def search_by_rid(self, rid):
@@ -14,11 +12,11 @@ class CommentQuerySet(QuerySet):
 class Comment(Document):
     """docstring for Comment"""
     meta = {
-        'collection': 'restaurant',
+        'collection': 'comment',
         'queryset_class': CommentQuerySet,
         'strict': False,
     }
     uid = StringField(required=True)
-    rid = LongField(required=True, min_value=0, max_value=1)
+    rid = IntField(required=True)
     rate = FloatField(required=True, min_value=0, max_value=1)
     context = StringField()

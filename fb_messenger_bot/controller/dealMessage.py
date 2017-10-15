dc2fabc31725bfc0b464b4aae40615b3a33cac5d
aspirine1a <qweaspirine@gmail.com>
2017-10-15 12:06:11 +0800

    commit python
    commit python

--- fb_messenger_bot/controller/dealMessage.py
+++ fb_messenger_bot/controller/dealMessage.py
@@ -1,6 +1,5 @@
 from model.restaurant import *
-from model.userProfile import *
-import datetime
+
 
 class DealMessage(object):
     """docstring for DealMessage"""
@@ -16,8 +15,8 @@ class DealMessage(object):
         self.catalog = "[" + cata + "]"
 
     def set_cost(self, min_cost=0, max_cost=100):
-        self.min_cost = min_cost
-        self.max_cost = max_cost
+        self.min_cost = int(min_cost)
+        self.max_cost = int(max_cost)
 
     def set_location(self, location):
         self.location = location
@@ -25,27 +24,22 @@ class DealMessage(object):
     def set_distance(self, distance):
         self.distance = distance
 
-    def get_restaurant(self, limit=10):
+    def get_restaurant(self):
         result = None
         if self.location is not None:
             if self.catalog is not None:
                 if self.max_cost is None:
-                    result = Restaurant.objects.search_by_catalog(self.location, self.catalog, self.distance)[:limit]
+                    result = Restaurant.objects.search_by_catalog(self.location, self.catalog, self.distance)
                 else:
                     result = Restaurant.objects.search_by_avgCost_catalog(self.location, self.catalog, self.min_cost,
-                                                                  self.max_cost, self.distance)[:limit]
+                                                                  self.max_cost, self.distance)
             else :
                 if self.max_cost is None:
-                    result = Restaurant.objects.search_by_address(self.location, self.distance)[:limit]
+                    result = Restaurant.objects.search_by_address(self.location, self.distance)
                 else:
                     result = Restaurant.objects.search_by_avgCost(self.location, self.min_cost,
-                                                                  self.max_cost, self.distance)[:limit]
+                                                                  self.max_cost, self.distance)
         return result
 
     def get_all_catalog(self):
         return Restaurant.objects.get_catalog()
-
-    def save_search_set(self):
-        UserProfile.objects.insert_or_update(self.sender, [self.min_cost, self.max_cost],
-                                             self.catalog, self.location, self.distance)
-    
--- fb_messenger_bot/decide.py
+++ fb_messenger_bot/decide.py
@@ -1,6 +1,10 @@
 from imports_send import *
 from model.restaurant import *
 from controller.dealMessage import *
+import pymongo
+
+client= pymongo.MongoClient(host=settings.host,port=settings.port)
+# db=client.
 restaurant=Restaurant()
 
 def log(message):
@@ -13,6 +17,8 @@ class decide(object):
         pass
     def process(self, request):
         data = request.get_json()
+        
+        location_tuple=(0,0)
         if data["object"] == "page":
             for entry in data["entry"]:
                 for messaging_event in entry["messaging"]:
@@ -21,6 +27,7 @@ class decide(object):
                     if messaging_event.get("message"):  # someone sent us a message
                         sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                         recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
+                        dealMessage=DealMessage(sender_id)
                         if "text" not in messaging_event["message"]:
                             typeText=""
                             for attachments in messaging_event["message"]["attachments"]:
@@ -28,17 +35,28 @@ class decide(object):
                             print(typeText)
                             if typeText== "location":
                                 diction = dict()
-                                dealMessage=DealMessage(sender_id)
-                                dealMessage.set_location((121.56008359778, 25.080193176667))
                                 for attachments in messaging_event["message"]["attachments"]:
                                     diction[sender_id]=attachments["payload"]["coordinates"]
                                 print(diction[sender_id],sender_id)
                                 location_tuple=(diction[sender_id]['long'],diction[sender_id]['lat'])
-                                send_message(sender_id,str(location_tuple))
+                                print(location_tuple[0])
+                                dealMessage.set_distance(1500)
+                                dealMessage.set_location((location_tuple[0],location_tuple[1]))
+                                send_message(sender_id,str(location_tuple)+"aaaaa")
+                                text=[]
+                                cost=[]
                                 for i in dealMessage.get_restaurant():
-                                    print(str(i.name))
-                                    send_message(sender_id,str(i.name))
+                                    text.append(i.name)
+                                    cost.append(i.avgCost)
+                                send_generic(sender_id,text,cost)
+                                print(cost)
+                                    # print(str(i.name))
+                                    # print("==================")
+                                print("text append"+str(text))
+                                    # send_message(sender_id,str(i.name))
+                                
                                 send_message(sender_id, "location finished")
+
                             #elif typeText=="image":
 
                         else:
@@ -51,13 +69,31 @@ class decide(object):
                                 send_quicky_buget(sender_id)
                             elif message_text == "restaurant catalog":
                                 send_message(sender_id, "this is your catalog")
-                            #just testing function 
+                            elif '~' in message_text :
+                                under,upper= message_text.split("~")
+                                # dealMessage.set_location((121.56008359778, 25.080193176667))
+                                print("is get location ?"+str(location_tuple[0]))
+                                dealMessage.set_location((location_tuple[0],location_tuple[1]))
+                                dealMessage.set_cost(under,upper)
+                                for i in dealMessage.get_restaurant():
+                                    print(i.name)
+                                text=[]
+                                cost=[]
+                                for i in dealMessage.get_restaurant():
+                                    text.append(i.name)
+                                    cost.append(i.avgCost)
+                                send_generic(sender_id,text,cost)
+                                send_message(sender_id,"got $$"+under)
+                            elif ':' in message_text:
+                                key,value= message_text.split(":")
+                            # # just testing function 
                             # elif message_text == "news":
-                            #     send_button(sender_id, "this is news location we recommend")
+                            #     dealMessage.set_distance
+                                # send_button(sender_id, "this is news location we recommend")
                             elif message_text =='image':
                                 send_image(sender_id)
-                            elif message_text == 'list':
-                                send_generic(sender_id)
+                            # elif message_text == 'list':
+                            #     send_generic(sender_id)
                             else :
                                 quick_text="we do not know this text:\n"+message_text+"\nHere's a quick reply!"
 

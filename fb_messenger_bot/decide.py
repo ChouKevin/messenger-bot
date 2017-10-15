d8efc819066a1c921444b3f8191ff8d57c11d46f
aspirine1a <qweaspirine@gmail.com>
2017-10-15 12:07:45 +0800

    Revert "commit python"
    This reverts commit dc2fabc31725bfc0b464b4aae40615b3a33cac5d.

--- fb_messenger_bot/decide.py
+++ fb_messenger_bot/decide.py
@@ -1,10 +1,6 @@
 from imports_send import *
 from model.restaurant import *
 from controller.dealMessage import *
-import pymongo
-
-client= pymongo.MongoClient(host=settings.host,port=settings.port)
-# db=client.
 restaurant=Restaurant()
 
 def log(message):
@@ -17,8 +13,6 @@ class decide(object):
         pass
     def process(self, request):
         data = request.get_json()
-        
-        location_tuple=(0,0)
         if data["object"] == "page":
             for entry in data["entry"]:
                 for messaging_event in entry["messaging"]:
@@ -27,7 +21,6 @@ class decide(object):
                     if messaging_event.get("message"):  # someone sent us a message
                         sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                         recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                        dealMessage=DealMessage(sender_id)
                         if "text" not in messaging_event["message"]:
                             typeText=""
                             for attachments in messaging_event["message"]["attachments"]:
@@ -35,28 +28,17 @@ class decide(object):
                             print(typeText)
                             if typeText== "location":
                                 diction = dict()
+                                dealMessage=DealMessage(sender_id)
+                                dealMessage.set_location((121.56008359778, 25.080193176667))
                                 for attachments in messaging_event["message"]["attachments"]:
                                     diction[sender_id]=attachments["payload"]["coordinates"]
                                 print(diction[sender_id],sender_id)
                                 location_tuple=(diction[sender_id]['long'],diction[sender_id]['lat'])
-                                print(location_tuple[0])
-                                dealMessage.set_distance(1500)
-                                dealMessage.set_location((location_tuple[0],location_tuple[1]))
-                                send_message(sender_id,str(location_tuple)+"aaaaa")
-                                text=[]
-                                cost=[]
+                                send_message(sender_id,str(location_tuple))
                                 for i in dealMessage.get_restaurant():
-                                    text.append(i.name)
-                                    cost.append(i.avgCost)
-                                send_generic(sender_id,text,cost)
-                                print(cost)
-                                    # print(str(i.name))
-                                    # print("==================")
-                                print("text append"+str(text))
-                                    # send_message(sender_id,str(i.name))
-                                
+                                    print(str(i.name))
+                                    send_message(sender_id,str(i.name))
                                 send_message(sender_id, "location finished")
-
                             #elif typeText=="image":
 
                         else:
@@ -69,31 +51,13 @@ class decide(object):
                                 send_quicky_buget(sender_id)
                             elif message_text == "restaurant catalog":
                                 send_message(sender_id, "this is your catalog")
-                            elif '~' in message_text :
-                                under,upper= message_text.split("~")
-                                # dealMessage.set_location((121.56008359778, 25.080193176667))
-                                print("is get location ?"+str(location_tuple[0]))
-                                dealMessage.set_location((location_tuple[0],location_tuple[1]))
-                                dealMessage.set_cost(under,upper)
-                                for i in dealMessage.get_restaurant():
-                                    print(i.name)
-                                text=[]
-                                cost=[]
-                                for i in dealMessage.get_restaurant():
-                                    text.append(i.name)
-                                    cost.append(i.avgCost)
-                                send_generic(sender_id,text,cost)
-                                send_message(sender_id,"got $$"+under)
-                            elif ':' in message_text:
-                                key,value= message_text.split(":")
-                            # # just testing function 
+                            #just testing function 
                             # elif message_text == "news":
-                            #     dealMessage.set_distance
-                                # send_button(sender_id, "this is news location we recommend")
+                            #     send_button(sender_id, "this is news location we recommend")
                             elif message_text =='image':
                                 send_image(sender_id)
-                            # elif message_text == 'list':
-                            #     send_generic(sender_id)
+                            elif message_text == 'list':
+                                send_generic(sender_id)
                             else :
                                 quick_text="we do not know this text:\n"+message_text+"\nHere's a quick reply!"
 
--- fb_messenger_bot/send_generic.py
+++ fb_messenger_bot/send_generic.py
@@ -2,40 +2,7 @@ from imports import *
 import csv
 
 
-def send_generic(recipient_id,text,cost):
-    a=[]
-
-    # for i in text:
-    #     print(str(i))
-    x=-1
-    data_len=11
-    if len(text)<11 :
-        data_len=len(text)
-    for i in range(0,data_len):
-        a.append(json.dumps({
-            "title": text[i],
-            "image_url": "http://iphoto.ipeen.com.tw/photo/ipeen/200x200/def/8/4/8/838848/838848_20150412173830_6101.jpg",
-            "subtitle": cost[i],
-            "default_action": {
-                "type": "web_url",
-                "url": "https://dinein.kfc.com.sg/",
-                "webview_height_ratio": "tall"
-            },
-            "buttons": [
-                {
-                    "title": "more info",
-                    "type": "web_url",
-                    "url": "https://dinein.kfc.com.sg/",
-                    "webview_height_ratio": "tall"
-                },
-                {
-                    "title": "menu",
-                    "type": "web_url",
-                    "url": "https://dinein.kfc.com.sg/our-food/",
-                    "webview_height_ratio": "tall"
-                }
-            ]
-        }))
+def send_generic(recipient_id):
     data = json.dumps({     
         "recipient": {
             "id": recipient_id
@@ -45,7 +12,50 @@ def send_generic(recipient_id,text,cost):
                 "type": "template",
                 "payload": {
                     "template_type": "generic",
-                    "elements": a
+                    "elements": [
+                        {
+                            "title": "Smurfs: The Lost Village (2017)",
+                            "image_url": "http://iphoto.ipeen.com.tw/photo/ipeen/200x200/def/8/4/8/838848/838848_20150412173830_6101.jpg",
+                            "subtitle": "KFC landed in Singapore in 1977, when the first restaurant opened its doors at Somerset Road.Today KFC serves more than 1 million customers each month through over 80 stores across Singapore. This makes KFC one of the largest fast food chains in the country.We are passionate about serving our customers freshly prepared, great tasting food with a key part of this being The Colonel's signature blend of 11 herbs and spices. Even today these remain a secret with the original recipe under lock and key in our headquarters in Kentucky, USA.",
+                            "default_action": {
+                                "type": "web_url",
+                                "url": "https://dinein.kfc.com.sg/",
+                                "webview_height_ratio": "tall"
+                            },
+                            "buttons": [
+                                {
+                                    "title": "more info",
+                                    "type": "web_url",
+                                    "url": "https://dinein.kfc.com.sg/",
+                                    "webview_height_ratio": "tall"
+                                },
+                               {
+                                    "title": "menu",
+                                    "type": "web_url",
+                                    "url": "https://dinein.kfc.com.sg/our-food/",
+                                    "webview_height_ratio": "tall"
+                                }
+                            ]
+                        },
+                        {
+                            "title": "Resident Evil: The Final Chapter (2017)",
+                            "image_url": "https://www.moovrika.com/ext/makeimg.php?tbl=movies&id=4167&img=1&type=image&movie=Resident+Evil+The+Final+Chapter&fs=400",
+                            "subtitle": "Resident Evil: The Final Chapter is an upcoming science fiction action horror film written and directed by Paul W. S. Anderson. It is the sequel to Resident Evil: Retribution (2012), and will be the sixth and final installment in the Resident Evil film series, which is very loosely based on the Capcom survival horror video game series Resident Evil.",
+                            "default_action": {
+                                "type": "web_url",
+                                "url": "https://www.moovrika.com/m/4167",
+                                "webview_height_ratio": "tall"
+                            },
+                            "buttons": [
+                                {
+                                    "title": "more info",
+                                    "type": "web_url",
+                                    "url": "https://www.moovrika.com/m/4082",
+                                    "webview_height_ratio": "tall"
+                                }
+                            ]
+                        }
+                    ]
                 }
             }
         }#end message

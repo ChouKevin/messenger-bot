3ed9069311fcc4d6699475fe7497cfc4dad9040b
aspirine1a <qweaspirine@gmail.com>
2017-10-16 12:04:14 +0800

    re push *.py
    re push *.py

---
+++ fb_messenger_bot/send_quicky_select.py
@@ -0,0 +1,22 @@
+from imports import *
+
+def send_quicky_select(recipient_id, text):
+    a = []
+
+    for i in range(0,9):
+        a.append(json.dumps({
+          "content_type":"text",
+          "title":str(i*100)+str("~")+str(i*100+100),
+          "payload":"<POSTBACK_PAYLOAD>"
+        }))
+    print(a)
+    data = json.dumps({     
+        "recipient": {
+            "id": recipient_id
+        },
+        "message":{
+            "text": "sccc",
+            "quick_replies":a
+        }
+    })
+    r=requests.post("https://graph.facebook.com/v2.6/me/messages", params=app_config["PARAMS"], headers=app_config["HEADERS"], data=data)
